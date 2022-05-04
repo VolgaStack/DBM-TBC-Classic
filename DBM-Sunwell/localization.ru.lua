993a2df837b07e59eb88390b4940aac5e723dfd7
Котов Дмитрий Михайлович <raziel.kdm@gmail.com>
2022-05-05 01:29:10 +0300

    Update localization.ru.lua

--- DBM-Sunwell/localization.ru.lua
+++ DBM-Sunwell/localization.ru.lua
@@ -11,39 +11,34 @@ L:SetGeneralLocalization{
 }
 
 L:SetWarningLocalization{
-	WarnPortal			= "Портал #%d : >%s< (Group %d)",
-	SpecWarnWildMagic	= "Wild Magic - %s!"--Translate
-}
-
-L:SetTimerLocalization{
-	TimerNextPortal		= "Портал (%d)"
+	WarnPortal			= "Портал #%d : >%s< (Группа %d)",
+	SpecWarnWildMagic	= "Дикая магия - %s!"
 }
 
 L:SetOptionLocalization{
-	WarnPortal			= "Show warning for $spell:46021 target",--Translate
-	SpecWarnWildMagic	= "Show special warning for Wild Magic",--Translate
-	TimerNextPortal		= "Show timer for portals",--Translate
-	RangeFrame			= "Show range frame (10 yards)",--Translate
-	ShowFrame			= "Show Spectral Realm frame" ,--Translate
-	FrameClassColor		= "Use class colors in Spectral Realm frame",--Translate
-	FrameUpwards		= "Expand Spectral Realm frame upwards",--Translate
-	FrameLocked			= "Set Spectral Realm frame not movable"--Translate
+	WarnPortal			= "Показывать предупреждение для цели $spell:46021",
+	SpecWarnWildMagic	= "Показывать специальные предупреждения для Дикой магии",
+	ShowFrame			= "Показывать фрейм Призрачного мира",
+	FrameClassColor		= "Использовать цвета классов в фрейме Призрачного мира",
+	FrameUpwards		= "расширять фрейм Призрачного мира вверх",
+	FrameLocked			= "Закрепить фрейм Призрачного мира на месте"
+	RangeFrame			= DBM_CORE_L.AUTO_RANGE_OPTION_TEXT:format(10, 46021)
 }
 
 L:SetMiscLocalization{
 	Demon				= "Сатроварр Осквернитель",
 	Heal				= "+100% хила",
 	Haste				= "+100% время каста",
-	Hit					= "-50% hit chance",
-	Crit				= "+100% крит дамага",
-	Aggro				= "+100% threat",
-	Mana				= "-50% spell costs",
-	FrameTitle			= "Spectral Realm",--Translate
+	Hit					= "-50% шанс попасть",
+	Crit				= "+100% крит урона",
+	Aggro				= "+100% угрозы",
+	Mana				= "-50% стоимости заклинаний",
+	FrameTitle			= "Призрачный мир",
 	FrameLock			= "Закрепить рамку",
-	FrameClassColor		= "Use class colors",--Translate
-	FrameOrientation	= "Expand upwards",--Translate
+	FrameClassColor		= "Использовать цвета классов",
+	FrameOrientation	= "Расширять вверх",
 	FrameHide			= "Скрыть рамку",
-	FrameClose			= "Close"--Translate
+	FrameClose			= "Закрыть"
 }
 
 ----------------
@@ -52,11 +47,11 @@ L:SetMiscLocalization{
 L = DBM:GetModLocalization("Brutallus")
 
 L:SetGeneralLocalization{
-	name = "Бруталл"
+	name				= "Бруталл"
 }
 
 L:SetMiscLocalization{
-	Pull			= "О, а вот и новые агнцы идут на заклание!"
+	Pull				= "Аа, новые овечки на заклание?"
 }
 
 --------------
@@ -69,33 +64,26 @@ L:SetGeneralLocalization{
 }
 
 L:SetWarningLocalization{
-	WarnPhase		= "%s Phase",--Translate
-	WarnPhaseSoon	= "%s Phase in 10 sec",--Translate
-	WarnBreath		= "Глубокий Вздох (%d)"
+	WarnPhase		= "%s фаза",
 }
 
 L:SetTimerLocalization{
-	TimerPhase		= "Next %s Phase",
-	TimerBreath		= "Глубокий Вздох"
+	TimerPhase		= "Следующая %s фаза",
 }
 
 L:SetOptionLocalization{
-	WarnPhase		= "Show warning for next phase",--Translate
-	WarnPhaseSoon	= "Show pre-warning for next phase",--Translate
-	WarnBreath		= "Show warning for Deep Breath",--Translate
-	TimerPhase		= "Show time for next phase",--Translate
-	TimerBreath		= "Show timer for Deep Breath cooldown",--Translate
+	WarnPhase		= "Показывать предупреждение для следующей фазы",
+	TimerPhase		= "Показывать таймер до следующей фазы",
 	VaporIcon		= DBM_CORE_L.AUTO_ICONS_OPTION_TEXT:format(45392),
-	EncapsIcon		= DBM_CORE_L.AUTO_ICONS_OPTION_TEXT:format(45665),
-	YellOnEncaps	= "Yell on $spell:45665"
+	EncapsIcon		= DBM_CORE_L.AUTO_ICONS_OPTION_TEXT:format(45665)
+
 }
 
 L:SetMiscLocalization{
-	Air				= "Air",--Translate
-	Ground			= "Ground",--Translate
-	YellEncaps		= "Encapsulate on me! Run away!",--Change to generic so we don't have to translate?
-	AirPhase		= "I am stronger than ever before!",--Translate
-	Breath			= "%s глубоко вздыхает."
+	Air				= "Наземная",
+	Ground			= "Воздушная",
+	AirPhase		= "Я сильнее, чем когда-либо прежде!",
+	Breath			= "%s глубоко вдыхает."
 }
 
 -----------------------
@@ -107,26 +95,19 @@ L:SetGeneralLocalization{
 	name = "Близнецы"
 }
 
-L:SetWarningLocalization{
-}
-
-L:SetTimerLocalization{
-}
-
 L:SetOptionLocalization{
 	NovaIcon		= DBM_CORE_L.AUTO_ICONS_OPTION_TEXT:format(45329),
 	ConflagIcon		= DBM_CORE_L.AUTO_ICONS_OPTION_TEXT:format(45333),
-	RangeFrame		= "Show range frame (10 yards)",--Translate
-	NovaWhisper		= "Send whisper to $spell:45329 target (requires Raid Leader)",--Translate
-	ConflagWhisper	= "Send whisper to $spell:45333 target (requires Raid Leader)",--Translate
+	RangeFrame		= DBM_CORE_L.AUTO_RANGE_OPTION_TEXT:format(10, 45333)
 }
 
 L:SetMiscLocalization{
-	NovaWhisper		= "Shadow Nova on you!",--Translate
-	ConflagWhisper	= "Conflagration on you!",--Translate
-	Nova			= "Sacrolash directs Shadow Nova at (.+)%.",--Verify
-	Conflag			= "Алайтесс воздействует на"--Verify
+	Nova			= "заклинание Кольцо Тьмы на (.+)%",
+	Conflag			= "направляет \"Воспламенение\" на (.+)",
+	Sacrolash		= "Леди Сакролаш",
+	Alythess		= "Верховная чернокнижница Алитесса"
 }
+
 
 ------------
 --  M'uru --
@@ -134,32 +115,28 @@ L:SetMiscLocalization{
 L = DBM:GetModLocalization("Muru")
 
 L:SetGeneralLocalization{
-	name = "М'ару"
+	name = "М'ууру"
 }
 
 L:SetWarningLocalization{
-	WarnHuman		= "Humanoids (%d)",--Translate
-	WarnHumanSoon	= "Humanoids in 5 sec (%d)",--Translate
-	WarnVoid		= "Void Sentinel (%d)",--Translate
-	WarnVoidSoon	= "Void Sentinel in 5 sec (%d)",--Translate
-	WarnFiend		= "Dark Fiend spawned"--Translate
+	WarnHuman		= "Стража (%d)",
+	WarnVoid		= "Часовой Бездны (%d)",
+	WarnFiend		= "Исчадия тьмы!"
 }
 
 L:SetTimerLocalization{
-	TimerHuman		= "Humanoids (%s)",--Translate
-	TimerVoid		= "Leerenwandler (%s)",
+	TimerHuman		= "Стража (%s)",
+	TimerVoid		= "Часовой Бездны (%s)",
 	TimerPhase		= "Энтропий"
 }
 
 L:SetOptionLocalization{
-	WarnHuman		= "Show warning for Humanoids",--Translate
-	WarnHumanSoon	= "Show pre-warning for Humanoids",--Translate
-	WarnVoid		= "Show warning for Void Sentinels",--Translate
-	WarnVoidSoon	= "Show pre-warning for Void Sentinels",--Translate
-	WarnFiend		= "Show warning for Fiends in phase 2",--Translate
-	TimerHuman		= "Show timer for Humanoids",--Translate
-	TimerVoid		= "Show timer for Void Sentinels",--Translate
-	TimerPhase		= "Show time for Phase 2 transition"--Translate
+	WarnHuman		= "Показывает предупреждение о Страже",
+	WarnVoid		= "Показывает предупреждение о Часовом бездны",
+	WarnFiend		= "Показывает предупреждение о Исчадиях тьмы во второй фазе",
+	TimerHuman		= "Показывает таймер до Стражи",
+	TimerVoid		= "Показывает таймер до Часового Бездны",
+	TimerPhase		= "Показывает таймер до перехода во вторую фазу"
 }
 
 L:SetMiscLocalization{
@@ -187,24 +164,19 @@ L:SetTimerLocalization{
 }
 
 L:SetOptionLocalization{
-	WarnDarkOrb		= "Show warning for Dark Orbs",--Translate
-	WarnBlueOrb		= "Show warning for Dragon Orbs",--Translate
+	WarnDarkOrb		= "Показывает предупреждение о темных сферах",
+	WarnBlueOrb		= "Показывает предупреждение о сферах Калесгоса",
 	SpecWarnDarkOrb	= "Show special warning for Dark Orbs",--Translate
 	SpecWarnBlueOrb	= "Show special warning for Dragon Orbs",--Translate
 	TimerBlueOrb	= "Show timer form Dragon Orbs activate",--Translate
-	RangeFrame		= "Show range frame (10 yards)",--Translate
-	BloomIcon		= DBM_CORE_L.AUTO_ICONS_OPTION_TEXT:format(45641),
-	YellOnBloom		= "Yell on $spell:45641",--Translate
-	BloomWhisper	= "Send whisper to $spell:45641 target (requires Raid Leader)"--Translate
+	RangeFrame		= DBM_CORE_L.AUTO_RANGE_OPTION_TEXT:format(10, 45641),
+	BloomIcon		= DBM_CORE_L.AUTO_ICONS_OPTION_TEXT:format(45641)
 }
 
 L:SetMiscLocalization{
-	YellPull		= "Жалкие твари уничтожены. Да будет так! Я сделаю то, что не удалось Саргерасу. Я выпущу кровь из этого мерзкого мира и стану истинным повелителем Пылающего Легиона! Конец близок! Да свершится падение этого мира!",
-	YellBloom		= "Fire Bloom on me!",--Translate
-	BloomWhisper	= "Fire Bloom on you!",--Translate
-	OrbYell1		= "I will channel my powers into the orbs! Be ready!",--Translate
+	YellPull		= "Те, кем можно было пожертвовать, мертвы. Так тому и быть! Я добьюсь успеха там, где Саргерас потерпел поражение! Я заставлю этот жалкий мирок истекать кровью и навеки закреплю за собой место повелителя Пылающего Легиона! Пробил последний час этого мира!",
+	OrbYell1		= "Я наполню сферы своей энергией! Готовьтесь!",
 	OrbYell2		= "I have empowered another orb! Use it quickly!",--Translate
-	OrbYell3		= "Another orb is ready! Make haste!",--Translate
-	OrbYell4		= "I have channeled all I can! The power is in your hands!"--Translate
-
+	OrbYell3		= "Готова еще одна сфера! Торопитесь!",
+	OrbYell4		= "Я отдал все, что мог. Моя энергия в ваших руках!"
 }
